9b16e645c7810f4df470688bf7847df324653d03
daolotos <117892786+daolotos@users.noreply.github.com>
2022-11-26 16:40:34 +0300

    Решила 15 задачу

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Введите цифру дня недели: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 5)
+{
+    Console.WriteLine("нет, это не выходной");
+    
+}
+else Console.WriteLine("да, это выходной");
+
