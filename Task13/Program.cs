30ef9c7231c54fca66b94868ab2664e09d02aa89
daolotos <117892786+daolotos@users.noreply.github.com>
2022-11-26 16:17:22 +0300

    Задача 13 сделана

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 100)
+{
+    Console.WriteLine("Это не трехзначное число, попробуйте еще раз");
+    return;
+}
+while (N >= 1000)
+{
+    N = N / 10;
+}
+int b = N % 10;
+
+Console.WriteLine("Третье число:" + b);
+
